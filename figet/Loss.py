f43268e69be7d354d069ff35663ca2a331b97a75
Federico Lopez <fedelopez77@gmail.com>
2018-08-20 10:55:16 +0200

    Correction on the riemannian correction

--- figet/Loss.py
+++ figet/Loss.py
@@ -72,8 +72,13 @@ class PoincareDistance(Function):
         g = g.unsqueeze(-1)
         gu = PoincareDistance.grad(u, v, squnorm, sqvnorm, sqdist)
         gv = PoincareDistance.grad(v, u, sqvnorm, squnorm, sqdist)
-        return PoincareDistance.apply_riemannian_correction(g.expand_as(gu) * gu), \
-               PoincareDistance.apply_riemannian_correction(g.expand_as(gv) * gv)
+
+        grad_u = g.expand_as(gu) * gu
+        grad_v = g.expand_as(gv) * gv
+
+        cu, cv = PoincareDistance.apply_riemannian_correction(u, grad_u), \
+                 PoincareDistance.apply_riemannian_correction(v, grad_v)
+        return cu, cv
 
     @staticmethod
     def grad(x, v, sqnormx, sqnormv, sqdist):
@@ -87,24 +92,20 @@ class PoincareDistance(Function):
         return 4 * a / z.expand_as(x)
 
     @staticmethod
-    def apply_riemannian_correction(gradient, check_graph=False):
-        dimensions = gradient.dim()
-        grad_norm = torch.norm(gradient, 2, dimensions - 1, True)
+    def apply_riemannian_correction(point, gradient, check_graph=False):
+        p_sqnorm = torch.sum(point.data ** 2, dim=-1, keepdim=True)
+        corrected_gradient = gradient * ((1 - p_sqnorm) ** 2 / 4).expand_as(gradient)
+        return corrected_gradient.clamp(min=-10000.0, max=10000.0)
 
-        # This is the inverse of the Riemannian metric, which we need to correct for
-        hyper_b = (1 - grad_norm**2)**2 / 4
-        new_size = tuple([1] * (dimensions - 1) + [gradient.size(dimensions - 1)])
 
-        result = gradient * hyper_b.repeat(*new_size)  # multiply pointwise
-        return result.clamp(min=-10000.0, max=10000.0)
 
-        # return result
         #
+        # dimensions = gradient.dim()
+        # grad_norm = torch.norm(gradient, 2, dimensions - 1, True)
         #
-        # # We could do the projection here?
-        # # NB: THIS IS DEATHLY SLOW. FIX IT
-        # if check_graph and np.any(np.isnan(gradient.cpu().numpy())):
-        #     print(np.any(np.isnan(gradient.numpy())))
-        #     print(np.any(np.isnan(gradient.cpu().numpy())))
-        #     print(np.any(np.isnan(grad_norm.cpu().numpy())))
-        #     raise ValueError("NaN During Hyperbolic")
+        # # This is the inverse of the Riemannian metric, which we need to correct for
+        # hyper_b = (1 - grad_norm**2)**2 / 4
+        # new_size = tuple([1] * (dimensions - 1) + [gradient.size(dimensions - 1)])
+        #
+        # result = gradient * hyper_b.repeat(*new_size)  # multiply pointwise
+        # return result.clamp(min=-10000.0, max=10000.0)
