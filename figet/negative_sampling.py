6bed872966109ae0e98714e94b623a1b9c3a12e1
Federico Lopez <fedelopez77@gmail.com>
2018-08-23 16:37:50 +0200

    Adding cache to negative sampling

--- figet/negative_sampling.py
+++ figet/negative_sampling.py
@@ -14,25 +14,38 @@ class NegativeSampleContainer(object):
             ordered_idx_and_dist = sorted(enumerate(distances), key=itemgetter(1), reverse=True)
             self.index_and_distance[idx] = ordered_idx_and_dist
 
-    def get_indexes(self, idx, n, current_epoch=None, total_epochs=None):
-        """
-        :param n: amount of negative indexes
-        :param current_epoch: if is not None, returns the corresponding batch, to make training harder. Pre: epoch >= 1
-        :return: list of negative indexes
-        """
-        neg_batch = self._get_negative_batch(idx, current_epoch, total_epochs)
+        self.index_cache = {}
+        self.distance_cache = {}
+        self.current_epoch_cache = -1
 
-        return [item[0] for item in neg_batch[:n]]
+    def get_indexes(self, idx, n, current_epoch=None, total_epochs=None):
+        return self.update_caches(idx, n, current_epoch, total_epochs)[0]
 
     def get_distances(self, idx, n, current_epoch=None, total_epochs=None):
+        return self.update_caches(idx, n, current_epoch, total_epochs)[1]
+
+    def update_caches(self, idx, n, current_epoch=None, total_epochs=None):
         """
         :param n: amount of negative indexes
         :param current_epoch: if is not None, returns the corresponding batch, to make training harder. Pre: epoch >= 1
-        :return: list of distances to negative samples
+        :return: list of negative indexes, list of distances to negative samples
         """
+        if current_epoch != self.current_epoch_cache:
+            self.clear_cache()
+            self.current_epoch_cache = current_epoch
+
+        if idx in self.index_cache:
+            return self.index_cache[idx], self.distance_cache[idx]
+
         neg_batch = self._get_negative_batch(idx, current_epoch, total_epochs)
 
-        return [item[1] for item in neg_batch[:n]]
+        sub_neg_batch = neg_batch[:n]
+        indexes = [item[0] for item in sub_neg_batch]
+        distances = [item[1] for item in sub_neg_batch]
+
+        self.index_cache[idx] = indexes
+        self.distance_cache[idx] = distances
+        return indexes, distances
 
     def _get_negative_batch(self, idx, current_epoch=None, total_epochs=None):
         neg_batch = self.index_and_distance[idx]
@@ -40,3 +53,7 @@ class NegativeSampleContainer(object):
             neg_batch_size = int(len(neg_batch) / total_epochs)
             neg_batch = neg_batch[(current_epoch - 1) * neg_batch_size:]
         return neg_batch
+
+    def clear_cache(self):
+        self.index_cache = {}
+        self.distance_cache = {}
