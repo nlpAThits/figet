Printing right and wrong results after training

figet/Dataset.py
@@ -46,6 +46,7 @@ class Dataset(object):
         previous_ctx_tensor = torch.LongTensor(len(mentions), args.context_length).fill_(figet.Constants.PAD)
         next_ctx_tensor = torch.LongTensor(len(mentions), args.context_length).fill_(figet.Constants.PAD)
         type_tensor = torch.LongTensor(len(mentions), type_len)
+        mention_idx_tensor = torch.LongTensor(len(mentions), args.context_length).fill_(figet.Constants.PAD)
 
         bar = tqdm(desc="to_matrix_{}".format(type_len), total=len(mentions))
 
@@ -64,10 +65,12 @@ class Dataset(object):
                 reversed_data = torch.from_numpy(item.next_context.numpy()[::-1].copy())
                 next_ctx_tensor[i].narrow(0, args.context_length - item.next_context.size(0), item.next_context.size(0)).copy_(reversed_data)
 
+            mention_idx_tensor[i].narrow(0, 0, item.mention_idx.size(0)).copy_(item.mention_idx)
+
         bar.close()
 
         return [mention_tensor.contiguous(), previous_ctx_tensor.contiguous(), next_ctx_tensor.contiguous(), \
-               type_tensor.contiguous()]
+               type_tensor.contiguous(), mention_idx_tensor.contiguous()]
 
     def __len__(self):
         try:

figet/Mention.py
@@ -22,7 +22,8 @@ class Mention(object):
         self.context_length = args.context_length   # 10 by default
 
         self.types = self.type_idx()        # type index in vocab
-        self.mention = self.mention_idx()   # average of embeds forming the mention
+        self.mention_idx = self.get_mention_idx()
+        self.mention = self.mention_avg()   # average of embeds forming the mention
         if args.single_context == 1:
             self.context = self.context_idx()
         else:
@@ -30,24 +31,28 @@ class Mention(object):
             self.next_context = self.next_context_idx()
         self.tokens = None
 
-    def mention_idx(self):
-        words2vecs = [self.word2vec[self.vocabs[c.TOKEN_VOCAB].lookup(token, c.UNK)] for token in self.fields[c.HEAD].split()]
+    def mention_avg(self):
+        words2vecs = [self.word2vec[self.vocabs[c.TOKEN_VOCAB].lookup(token, c.PAD)] for token in self.fields[c.HEAD].split()]
         return torch.mean(torch.stack(words2vecs), dim=0).squeeze(0)
+
+    def get_mention_idx(self):
+        head = self.fields[c.HEAD].split()[:self.context_length]
+        return self.vocabs[c.TOKEN_VOCAB].convert_to_idx(head, c.PAD_WORD)
 
     def context_idx(self):
         context = (self.prev_context_words() + [c.PAD_WORD] + self.next_context_words())
-        return self.vocabs[c.TOKEN_VOCAB].convert_to_idx(context, c.UNK_WORD)
+        return self.vocabs[c.TOKEN_VOCAB].convert_to_idx(context, c.PAD_WORD)
 
     def prev_context_idx(self):
         prev_context = self.prev_context_words()
-        return self.vocabs[c.TOKEN_VOCAB].convert_to_idx(prev_context, c.UNK_WORD)
+        return self.vocabs[c.TOKEN_VOCAB].convert_to_idx(prev_context, c.PAD_WORD)
 
     def prev_context_words(self):
         return self.fields[c.LEFT_CTX].split()[-self.context_length:]
 
     def next_context_idx(self):
         next_context = self.next_context_words()
-        return self.vocabs[c.TOKEN_VOCAB].convert_to_idx(next_context, c.UNK_WORD)
+        return self.vocabs[c.TOKEN_VOCAB].convert_to_idx(next_context, c.PAD_WORD)
 
     def next_context_words(self):
         return self.fields[c.RIGHT_CTX].split()[:self.context_length]
@@ -72,3 +77,5 @@ class Mention(object):
         except AttributeError:
             del self.prev_context
             del self.next_context
+
+