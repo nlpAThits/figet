3551fb93aa281122c09eae5fdd69cdaa278a19c6
Federico Lopez <fedelopez77@gmail.com>
2018-09-12 09:52:41 +0200

    adding neighbors

--- figet/result_printer.py
+++ figet/result_printer.py
@@ -20,6 +20,9 @@ class ResultPrinter(object):
         self.args = args
 
     def show(self, n=2):
+        filters = [is_strictly_right, is_partially_right, is_totally_wrong]
+        collected = [[], [], []]
+
         for batch_index in range(len(self.data)):
             batch = self.data[batch_index]
             types = batch[3]
@@ -30,32 +33,37 @@ class ResultPrinter(object):
 
             results = assign_types(predictions, neighbor_indexes, types, self.hierarchy)
 
-            filters = [is_strictly_right, is_partially_right, is_totally_wrong]
-            log_titles = ["++ Strictly right:", "+ Partially right:", "-- Totally wrong:"]
             for i in range(len(filters)):
                 criteria = filters[i]
-                log.debug(log_titles[i])
                 to_show = []
                 for j in range(len(results)):
                     true, predicted = results[j]
                     if criteria(true, predicted):
-                        # mention_idx, prev_ctx, next_ctx, true, predicted
-                        to_show.append([batch[4][j], batch[1][j], batch[2][j], true, predicted])
+                        # mention_idx, prev_ctx, next_ctx, true, predicted, neighbors
+                        to_show.append([batch[4][j], batch[1][j], batch[2][j], true, predicted, neighbor_indexes[j][:3]])
                     if len(to_show) == n: break
 
-                self.print_results(to_show)
+                collected[i] += to_show
+
+        log_titles = ["\n\n++ Strictly right:", "\n\n+ Partially right:", "\n\n-- Totally wrong:"]
+        for i in range(len(filters)):
+            log.debug(log_titles[i])
+            self.print_results(collected[i])
 
     def print_results(self, to_show):
         unk = "@"
-        for mention, prev_ctx, next_ctx, true, predicted in to_show:
+        for mention, prev_ctx, next_ctx, true, predicted, neighbors in to_show:
             mention_words = " ".join([self.token_vocab.get_label_from_word2vec_id(i.item(), unk) for i in mention if i != 0])
             prev_words = " ".join([self.token_vocab.get_label_from_word2vec_id(i.item(), unk) for i in prev_ctx])
             next_words = " ".join([self.token_vocab.get_label_from_word2vec_id(i.item(), unk) for i in np.flip(next_ctx.cpu().numpy(), 0)])
 
             true_types = " ".join([self.type_vocab.get_label(i.item()) for i in true])
             predicted_types = " ".join([self.type_vocab.get_label(i.item()) for i in predicted])
+            neighbor_types = " ".join([self.type_vocab.get_label(i.item()) for i in neighbors])
 
-            log.debug(f"Left: '{prev_words}'\nMention: '{mention_words}'\nRight:'{next_words}'\nTrue: '{true_types}' - Predicted: {predicted_types}\n-----")
+            log.debug(f"Left: '{prev_words}'\nMention: '{mention_words}'\nRight:'{next_words}'\n"
+                      f"True: '{true_types}' - Predicted: {predicted_types}\n"
+                      f"Closest neighbors: {neighbor_types}\n*****")
 
 
 def is_strictly_right(true, predicted):
