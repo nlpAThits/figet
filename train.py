15e561fbe9b63cf1407ea09458db545f0912d526
Federico Lopez <fedelopez77@gmail.com>
2018-11-13 15:05:38 +0100

    Adding logging of new parameters

--- train.py
+++ train.py
@@ -119,7 +119,7 @@ def main():
     configs = itertools.product(proj_learning_rate, proj_weight_decay, proj_bias, proj_non_linearity,
                                 classif_learning_rate, classif_weight_decay, classif_bias, proj_dropout, classif_hidden_size,
                                 knn_metrics, classif_hidden_layers, cosine_factors, norm_factors, hyperdist_factors,
-                                proj_hidden_layers, proj_hidden_size, k_neighbors, hyper_lambda)
+                                proj_hidden_layers, proj_hidden_size, k_neighbors, hyper_lambda, neg_sample_values)
 
     best_macro_f1 = -1
     best_configs = []
@@ -143,7 +143,9 @@ def main():
         args.cosine_factor = config[11]
         args.norm_factor = config[12]
         args.hyperdist_factor = config[13]
+
         args.hyper_lambda = config[17]
+        args.negative_samples = config[18]
 
         args.neighbors = config[16]
 
@@ -191,7 +193,8 @@ def log_config(config):
     log.info(f"proj_lr:{config[0]}, proj_l2:{config[1]}, proj_bias:{config[2]}, proj_nonlin:{config[3]}, "
              f"proj_hidden_layers: {config[14]}, proj_hidden_size:{config[15]}, proj_dropout:{config[7]}, "
              f"classif_lr:{config[4]}, cl_l2:{config[5]}, cl_bias:{config[6]}, knn:{config[9]}, "
-             f"cosine_factor:{config[11]}, norm_factor:{config[12]}, hyperdist_factor:{config[13]}, neighbors: {config[16]}")
+             f"cosine_factor:{config[11]}, norm_factor:{config[12]}, hyperdist_factor:{config[13]}, "
+             f"neighbors: {config[16]}, hyper_lambda: {config[17]}, neg_samples: {config[18]}")
              # f", hidden_layers:{config[10]}, cl_dropout:{config[7]}, cl_hidden_size:{config[8]}, ")
 
 
