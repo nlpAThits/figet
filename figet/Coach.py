f77607c352972a02ad744eb88bddb8ab337a0da5
Federico Lopez <fedelopez77@gmail.com>
2018-11-14 15:48:27 +0100

    Separating training of projection and classifier

--- figet/Coach.py
+++ figet/Coach.py
@@ -42,16 +42,18 @@ class Coach(object):
         log.debug(self.classifier)
 
         self.start_time = time.time()
-        # train_subsample = self.train_data.subsample(2000)
 
         for epoch in range(1, self.args.epochs + 1):
-            train_model_loss, train_classif_loss = self.train_epoch(epoch)
+            train_model_loss = self.train_proj_epoch(epoch)
 
-            # self.validate_projection(train_subsample, "train", epoch)
             self.validate_projection(self.dev_data, "dev", epoch, plot=epoch == self.args.epochs)
 
-            log.info(f"Results epoch {epoch}: "
-                     f"TRAIN loss: model: {train_model_loss:.2f}, classif:{train_classif_loss:.5f}")
+            log.info(f"Results epoch {epoch}: TRAIN loss: model: {train_model_loss:.2f}")
+
+        for epoch in range(1, self.args.epochs + 1):
+            train_classif_loss = self.train_classif_epoch(epoch)
+
+            log.info(f"Results epoch {epoch}: Classif:{train_classif_loss:.5f}")
 
         self.result_printer.show()
 
@@ -72,7 +74,7 @@ class Coach(object):
 
         return raw_evaluate(test_results), test_eval, None
 
-    def train_epoch(self, epoch):
+    def train_proj_epoch(self, epoch):
         """:param epoch: int >= 1"""
         if self.args.extra_shuffle == 1:
             self.train_data.shuffle()
@@ -81,17 +83,52 @@ class Coach(object):
         total_model_loss, total_classif_loss, total_angles, total_pos_dist, total_euclid_dist, total_norms = [], [], [], [], [], []
         self.set_learning_rate(epoch)
         self.model.train()
-        self.classifier.train()
+        # self.classifier.train()
         for i in tqdm(range(niter), desc="train_epoch_{}".format(epoch)):
             batch = self.train_data[i]
             types = batch[5]
 
             self.model_optim.zero_grad()
             model_loss, type_embeddings, _, angles, dist_to_pos, euclid_dist = self.model(batch, epoch)
-            model_loss.backward(retain_graph=True)
+            model_loss.backward()
             if self.args.max_grad_norm >= 0:
                 clip_grad_norm_(self.model.parameters(), self.args.max_grad_norm)
             self.model_optim.step()
+
+            # Stats.
+            total_angles.append(angles)
+            total_pos_dist.append(dist_to_pos)
+            total_euclid_dist.append(euclid_dist)
+            total_norms.append(torch.norm(type_embeddings, p=2, dim=1))
+            total_model_loss.append(model_loss.item())
+
+        all_pos = torch.cat(total_pos_dist)
+        all_euclid = torch.cat(total_euclid_dist)
+        all_angles = torch.cat(total_angles)
+        all_pred_norm = torch.cat(total_norms)
+
+        log.debug(f"Train epoch {epoch}: d to pos: {all_pos.mean():0.2f} +- {all_pos.std():0.2f}, "
+                  f"Euclid dist: {all_euclid.mean():0.2f} +- {all_euclid.std():0.2f}, "
+                  f"Angles: {all_angles.mean():0.2f} +- {all_angles.std():0.2f}, "
+                  f"Norm:{all_pred_norm.mean():0.2f}+-{all_pred_norm.std():0.2f}\n"
+                  f"cos_fact:{self.args.cosine_factor}, norm_fact:{self.args.norm_factor}, "
+                  f"max norm:{all_pred_norm.max().item()}, min norm:{all_pred_norm.min().item()}")
+        return np.mean(total_model_loss)
+
+    def train_classif_epoch(self, epoch):
+        """:param epoch: int >= 1"""
+        if self.args.extra_shuffle == 1:
+            self.train_data.shuffle()
+
+        niter = self.args.niter if self.args.niter != -1 else len(self.train_data)  # -1 in train and len(self.train_data) is num_batches
+        total_model_loss, total_classif_loss, total_angles, total_pos_dist, total_euclid_dist, total_norms = [], [], [], [], [], []
+        # self.model.train()
+        self.classifier.train()
+        for i in tqdm(range(niter), desc="train_epoch_{}".format(epoch)):
+            batch = self.train_data[i]
+            types = batch[5]
+
+            model_loss, type_embeddings, _, angles, dist_to_pos, euclid_dist = self.model(batch, epoch)
 
             neighbor_indexes, one_hot_neighbor_types = self.knn.neighbors(type_embeddings, types, self.args.neighbors)
 
@@ -110,30 +147,18 @@ class Coach(object):
             total_model_loss.append(model_loss.item())
             total_classif_loss.append(classifier_loss.item())
 
-            # if (i + 1) % self.args.log_interval == 0:
-            #     norms = torch.norm(type_embeddings, p=2, dim=1)
-            #     mean_norm = norms.mean().item()
-            #     max_norm = norms.max().item()
-            #     min_norm = norms.min().item()
-            #     avg_model_loss, avg_classif_loss = np.mean(total_model_loss), np.mean(total_classif_loss)
-            #
-            #     log.debug("Epoch %2d | %5d/%5d | loss %6.4f | %6.0f s elapsed"
-            #         % (epoch, i+1, len(self.train_data), avg_model_loss + avg_classif_loss, time.time()-self.start_time))
-            #     log.debug(f"Model loss: {avg_model_loss}, Classif loss: {avg_classif_loss}")
-            #     log.debug(f"Mean batch norm: {mean_norm:0.2f}, max norm: {max_norm}, min norm: {min_norm}")
-
         all_pos = torch.cat(total_pos_dist)
         all_euclid = torch.cat(total_euclid_dist)
         all_angles = torch.cat(total_angles)
         all_pred_norm = torch.cat(total_norms)
 
-        log.debug(f"Train epoch {epoch}: d to pos: {all_pos.mean():0.2f} +- {all_pos.std():0.2f}, "
-                  f"Euclid dist: {all_euclid.mean():0.2f} +- {all_euclid.std():0.2f}, "
-                  f"Angles: {all_angles.mean():0.2f} +- {all_angles.std():0.2f}, "
-                  f"Norm:{all_pred_norm.mean():0.2f}+-{all_pred_norm.std():0.2f}\n"
-                  f"cos_fact:{self.args.cosine_factor}, norm_fact:{self.args.norm_factor}, "
-                  f"max norm:{all_pred_norm.max().item()}, min norm:{all_pred_norm.min().item()}")
-        return np.mean(total_model_loss), np.mean(total_classif_loss)
+        # log.debug(f"Train epoch {epoch}: d to pos: {all_pos.mean():0.2f} +- {all_pos.std():0.2f}, "
+        #           f"Euclid dist: {all_euclid.mean():0.2f} +- {all_euclid.std():0.2f}, "
+        #           f"Angles: {all_angles.mean():0.2f} +- {all_angles.std():0.2f}, "
+        #           f"Norm:{all_pred_norm.mean():0.2f}+-{all_pred_norm.std():0.2f}\n"
+        #           f"cos_fact:{self.args.cosine_factor}, norm_fact:{self.args.norm_factor}, "
+        #           f"max norm:{all_pred_norm.max().item()}, min norm:{all_pred_norm.min().item()}")
+        return np.mean(total_classif_loss)
 
     def validate_projection(self, data, name, epoch=None, plot=False):
         total_model_loss, total_pos_dist, total_euclid_dist, total_norms, total_angles = [], [], [], [], []
