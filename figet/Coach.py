cd8a391c70cb43dad7eced7fb2a6460c32b32afc
Federico Lopez <fedelopez77@gmail.com>
2019-01-23 15:42:22 +0100

    fixing argument

--- figet/Coach.py
+++ figet/Coach.py
@@ -33,7 +33,7 @@ class Coach(object):
         self.args = args
         self.word2vec = word2vec
         self.type2vec = type2vec
-        self.knn = kNN(type2vec)
+        self.knn = kNN(type2vec, args.knn_hyper)
         self.result_printer = ResultPrinter(test_data, vocabs, model, classifier, self.knn, hierarchy, args)
         self.config = config
 
