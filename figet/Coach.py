9ab05a1d5f94c45a32c8aa04ffe125629cab5c30
Federico Lopez <fedelopez77@gmail.com>
2019-02-18 18:03:08 +0100

    Adding tensorboard logging

--- figet/Coach.py
+++ figet/Coach.py
@@ -7,6 +7,7 @@ from torch.nn.utils import clip_grad_norm_
 import numpy as np
 from tqdm import tqdm
 from statistics import mean, stdev, median, mode, StatisticsError
+from tensorboardX import SummaryWriter
 
 from figet.utils import get_logging, plot_k
 from figet.Predictor import kNN, assign_types, assign_all_granularities_types
@@ -37,6 +38,7 @@ class Coach(object):
         self.result_printer = ResultPrinter(dev_data, vocabs, model, classifier, self.knn, hierarchy, args)
         self.config = config
         self.granularities = [COARSE_FLAG, FINE_FLAG, UF_FLAG]
+        self.writer = SummaryWriter(f"board/{args.exp_name}")
 
     def train(self):
         log.debug(self.model)
@@ -46,18 +48,27 @@ class Coach(object):
 
         for epoch in range(1, self.args.epochs + 1):
             train_model_loss = self.train_epoch(epoch)
+            self.writer.add_scalar("train/epoch_loss", train_model_loss, epoch)
 
             log.info(f"Results epoch {epoch}: TRAIN loss: model: {train_model_loss:.2f}")
 
             results, _ = self.validate_typing(self.dev_data, "dev", epoch)
             _, coarse_results = stratified_evaluate(results[0], self.vocabs[TYPE_VOCAB])
-            coarse_macro_f1 = float(coarse_results.split()[5])
+            coarse_split = coarse_results.split()
+            coarse_macro_f1 = float(coarse_split[5])
+
+            self.writer.add_scalar("dev_strict_f1", float(coarse_split[2]), epoch)
+            self.writer.add_scalar("dev_macro_f1", coarse_macro_f1, epoch)
+            self.writer.add_scalar("dev_micro_f1", float(coarse_split[8]), epoch)
 
             if coarse_macro_f1 > max_coarse_macro_f1:
                 max_coarse_macro_f1 = coarse_macro_f1
                 best_model_state = copy.deepcopy(self.model.state_dict())
                 best_epoch = epoch
                 log.info(f"* Best coarse macro F1 {coarse_macro_f1:0.2f} at epoch {epoch} *")
+
+            for name, param in self.model.named_parameters():
+                self.writer.add_histogram(name, param.clone().cpu().data.numpy(), epoch)
 
         log.info(f"Final evaluation on best coarse macro F1 ({max_coarse_macro_f1}) from epoch {best_epoch}")
         self.model.load_state_dict(best_model_state)
@@ -69,6 +80,8 @@ class Coach(object):
 
         self.validate_all_neighbors(self.test_data, "test", plot=True)
         coarse_results = self.print_full_validation(self.test_data, "test")
+
+        self.writer.close()
 
         return coarse_results
 
@@ -158,6 +171,8 @@ class Coach(object):
 
             self.print_stats(stats, name, epoch)
             log.debug(f"{name} loss: {np.mean(total_model_loss)}")
+            if epoch != -1:
+                self.writer.add_scalar(f"{name}/epoch_loss", np.mean(total_model_loss), epoch)
 
             return results, total_result
 
@@ -169,6 +184,13 @@ class Coach(object):
                 f"Euclid: {mean(item[2]):0.2f} +- {stdev(item[2]):0.2f}, "
                 f"Angles: {mean(item[0]):0.2f} +- {stdev(item[0]):0.2f}, "
                 f"Mean norm:{mean(item[3]):0.2f}+-{stdev(item[3]):0.2f}")
+
+            if epoch != -1:
+                prefix = f"Proj/{name}_{labels[idx]}"
+                self.writer.add_scalar(f"{prefix}_d_to_pos", mean(item[1]), epoch)
+                self.writer.add_scalar(f"{prefix}_euclid", mean(item[2]), epoch)
+                self.writer.add_scalar(f"{prefix}_angles", mean(item[0]), epoch)
+                self.writer.add_scalar(f"{prefix}_norm", mean(item[3]), epoch)
 
     def validate_all_neighbors(self, data, name, plot=False):
         """Warning: this function is very slow because it evaluates over all types on the dataset"""
