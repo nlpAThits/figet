816861fe15ecc192ea32b166f0193fd551b310b6
Federico Lopez <fedelopez77@gmail.com>
2019-02-22 18:09:35 +0100

    pred or true

--- plot-embeds.py
+++ plot-embeds.py
@@ -136,13 +136,13 @@ def main():
                     pred_and_true.append((predicted_embeds[0][j], predicted_embeds[1][j], predicted_embeds[2][j],
                                           types[j]))
 
-        tensors, metadata = [], ["ids\tgran\tlabel"]          # meta: id - granularity - pred|true
+        tensors, metadata = [], ["ids\tgran\tlabel\tis_pred"]          # meta: id - granularity - pred|true
         labels = ["coarse", "fine", "ultrafine"]
         for i in range(len(pred_and_true)):
             item_id = f"id-{i}"
             for j in range(3):
                 tensors.append("\t".join(map(str, pred_and_true[i][j].tolist())))
-                metadata.append(f"{item_id}\t{labels[j]}\tprediction")
+                metadata.append(f"{item_id}\t{labels[j]}\tnone\tpred")
             type_ids = pred_and_true[i][3].tolist()
             coarses = [i for i in type_ids if i in coarse_ids]
             fines = [i for i in type_ids if i in fine_ids and i not in coarse_ids]
@@ -151,7 +151,7 @@ def main():
                 for t_id in type_ids_by_gran:
                     tensors.append("\t".join(map(str, type2vec[t_id].tolist())))
                     true_label = vocabs[TYPE_VOCAB].idx2label[t_id]
-                    metadata.append(f"{item_id}\t{labels[gran]}\t{true_label}")
+                    metadata.append(f"{item_id}\t{labels[gran]}\t{true_label}\ttrue")
 
         path = "img/plot/pred-and-true"
         export(path + ".tsv", tensors)
