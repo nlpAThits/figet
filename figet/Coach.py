Correct normalization

figet/Coach.py
@@ -67,9 +67,9 @@ class Coach(object):
             for name, param in self.model.named_parameters():
                 self.writer.add_histogram(name, param.clone().cpu().data.numpy(), epoch)
 
-            if epoch % 15 == 0:
+            if epoch % 30 == 0:
                 self.print_full_validation(self.dev_data, f"epoch-{epoch}-dev")
-                torch.save(self.model.state_dict(), f"models/sep-grad-corrected-dict-{epoch}.pt")
+                torch.save(self.model.state_dict(), f"models/sep-grad-corrected-lr1p0-dict-{epoch}.pt")
 
         log.info(f"Final evaluation on best coarse macro F1 ({max_coarse_macro_f1}) from epoch {best_epoch}")
         self.model.load_state_dict(best_model_state)

figet/hyperbolic.py
@@ -123,8 +123,4 @@ def normalize(predicted_emb):
     """
     Projects the embedding with a norm above 1 (one) inside the Poincare ball
     """
-    norms = torch.sqrt(torch.sum(predicted_emb * predicted_emb, dim=-1))
-    ok_norms = norms < 1
-    inverses = 1.0 / (norms * (1 + EPS))
-    inverses[ok_norms] = 1.0
-    return predicted_emb * inverses.unsqueeze(-1)
+    return torch.renorm(predicted_emb, 2, 0, 1 - EPS)