33180c4e036bd9b8ed923b269f1199d362f12329
Federico Lopez <fedelopez77@gmail.com>
2019-04-12 12:31:15 +0200

    Hyperbolic Predictor again

--- figet/Predictor.py
+++ figet/Predictor.py
@@ -70,7 +70,7 @@ class kNN(object):
 
         predictions_numpy = predictions.detach().cpu().numpy()
 
-        factor = 2
+        factor = 50
         requested_neighbors = factor * k if factor * k <= max_neighbors else max_neighbors
         for i in range(3):
             try:
@@ -85,11 +85,11 @@ class kNN(object):
         result = []
         for x in range(len(mapped_indexes)):
             idx = mapped_indexes[x]
-            #predicted = predictions[x]
+            predicted = predictions[x]
 
-            #idx_and_tensors = list(zip(idx, [tensor for tensor in self.type2vec[idx]]))
-            #idx_and_distance = [(idx, poincare_distance(predicted, tensor)) for idx, tensor in idx_and_tensors]
-            #sorted_idx_and_tensors = sorted(idx_and_distance, key=itemgetter(1))
+            idx_and_tensors = list(zip(idx, [tensor for tensor in self.type2vec[idx]]))
+            idx_and_distance = [(idx, poincare_distance(predicted, tensor)) for idx, tensor in idx_and_tensors]
+            sorted_idx_and_tensors = sorted(idx_and_distance, key=itemgetter(1))
             result.append(idx[:k])
 
         return torch.LongTensor(result).to(self.device)
